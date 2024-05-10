55d95c96af4cda9355c2659d63a0e250ab97c49d
Argenis De La Rosa <theonlyhennygod@gmail.com>
2024-05-10 14:35:20 -0400

    finished routes for dalle and posts

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,16 +1,14 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import { Configuration, OpenAIApi } from 'openai';
+import OpenAI from 'openai';
 
 dotenv.config();
 
 const router = express.Router();
 
-const configuration = new Configuration({
-    apiKey: process.env.OPEN_AI_KEY,
-  });
-
-const openai = new OpenAIApi(configuration);
+const openai = new OpenAI({
+    apiKey: process.env.OPEN_API_KEY,
+});
 
 router.route('/').get((request, response) => {
     response.send('Hello from DALL-E AI');
@@ -18,22 +16,21 @@ router.route('/').get((request, response) => {
 
 router.route('/').post(async (request, response) => {
     try {
-        const { prompt }  = request.body;
+        const { prompt } = request.body;
 
-        const aiResponse = await openai.createImage({
+        const aiResponse = await openai.images.generate({
             prompt: prompt,
             n: 1,
             size: '1024x1024',
             response_format: 'b64_json'
         });
 
-        const image = aiResponse.data.data[0].b64_json;
-
+        const image = aiResponse.data[0].b64_json;
         response.status(200).json({ photo: image });
 
-        } catch (error) {
-            console.log('Error in generating image', error);
-            response.status(500).send({ message: 'Error in generating image' });
+    } catch (error) {
+        console.log('Error in generating image', error);
+        response.status(500).send({ message: 'Error in generating image' });
     }
 });
 
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -8,4 +8,47 @@ dotenv.config();
 
 const router = express.Router();
 
+// configure cloudinary
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+// get all posts
+
+router.route('/').get(async (request, response) => {
+    try {
+        const posts = await Post.find({});
+        response.status(200).json(posts);
+    } catch (error) { 
+        console.log('Error in getting posts', error);
+        response.status(500).json({ success: false, message: error });
+    }
+});
+
+// create a post
+
+router.route('/').post(async (request, response) => {
+    try {
+        const { name, prompt, photo } = request.body;
+        const result = await cloudinary.uploader.upload(photo, {
+            upload_preset: 'dalle'
+        });
+
+        const newPost = new Post({
+            name,
+            prompt,
+            photo: result.secure_url
+        });
+
+        await newPost.save();
+        response.status(201).json(newPost);
+    } catch (error) {
+        console.log('Error in creating post', error);
+        response.status(500).send({ message: 'Error in creating post' });
+    }
+})
+
 export default router; 
